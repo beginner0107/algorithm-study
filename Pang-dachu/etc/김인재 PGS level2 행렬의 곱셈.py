274d923c812f32cf3c19572d9687e7df4f401000
팡다츄 <fox3931@gmail.com>
2023-05-01 00:46:01 +0900

    Create code : 김인재 PGS level2 행렬의 곱셈

---
+++ Pang-dachu/etc/김인재 PGS level2 행렬의 곱셈.py
@@ -0,0 +1,9 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/12949
+
+import numpy as np  
+
+def solution(arr1, arr2):
+    temp = np.array(aarr1) @ np.array(arr2)
+    return temp.tolist()
+    
+    
